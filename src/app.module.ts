b8a9a15db3677a5299a3b7f3920e06a2c7423b0f
mihntam <minhtam.tm.dev@gmail.com>
2024-10-15 20:37:35 +0700

    add: user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './features/user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true, expandVariables: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true, expandVariables: true }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/setup-swagger.ts
+++ src/setup-swagger.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-export function setupSwagger(app: INestApplication) {
+export function setupSwagger(app: INestApplication): void {
   const documentBuilder = new DocumentBuilder()
     .setTitle('Base api project')
     .setDescription('## description will be update later')
@@ -10,11 +10,11 @@ export function setupSwagger(app: INestApplication) {
     .build();
 
   const document = SwaggerModule.createDocument(app, documentBuilder);
-  SwaggerModule.setup('documentation', app, document, {
+  SwaggerModule.setup('swagger', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
   });
 
-  console.info(`documentation: http://localhost:${process.env.PORT}/documentation`);
+  console.info(`documentation: http://localhost:${process.env.PORT}/swagger`);
 }
