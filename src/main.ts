22fb356eb0085935924e83a62647e71dc6f45f14
mihntam <minhtam.tm.dev@gmail.com>
2024-10-16 20:11:45 +0700

    use: global validation pipe & http filter

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { setupSwagger } from './setup-swagger';
+import { HttpExceptionFilter } from './utils';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   setupSwagger(app);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 
