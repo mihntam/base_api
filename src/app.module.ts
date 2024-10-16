d49c6bfa13d4e3ece4cc50ab9dd5bd9001df7689
Minh Tâm <minhtam.tm.dev@gmail.com>
2024-10-16 22:23:24 +0700

    Setup: minor things for the project (#1)
    * add: swagger setup
    
    * update package
    
    * add: user module
    
    * add: class validator & class transformer
    
    * use: global validation pipe & http filter
    
    * minor: lint, etc
    
    * implement(logger): winston log
    
    * minor changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './features/user/user.module';
+import { LoggerModule } from './features/logger/logger.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true, expandVariables: true }), UserModule, LoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { setupSwagger } from './setup-swagger';
+import { HttpExceptionFilter } from './utils';
+import { LoggerService } from './features/logger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(3000);
+
+  setupSwagger(app);
+
+  app.useLogger(app.get(LoggerService));
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+
+  console.info(`server running on ${await app.getUrl()}`);
 }
 bootstrap();
